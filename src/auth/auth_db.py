9178dbb7ce84e2367e5c670241e27e53cd84d801
AlejandroD999 <alejandrodgonzales923@gmail.com>
2026-05-11 20:10:36 -0500

    Renamed register_user -> add_user

--- src/auth/auth_db.py
+++ src/auth/auth_db.py
@@ -1,7 +1,7 @@
 from ..extensions import db
 from ..models.auth_mod import User
 
-def register_user(username, password):
+def add_user(username, password):
     new_user = User(username=username, password=password)
 
     try:
@@ -15,4 +15,5 @@ def fetch_user_id(name):
     user_data = db.session.query(User).filter_by(username=name).first()
 
     return user_data.id if user_data else None
+    
 
