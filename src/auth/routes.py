ccd2ec1f157dc9402183514c06ebca8e1ba6d471
AlejandroD999 <alejandrodgonzales923@gmail.com>
2026-05-13 19:59:32 -0500

    Renamed auth.py -> routes.py

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -3,9 +3,13 @@ from .auth_db import add_user, user_exists
 def register(username, password):
     # Manage user registration
 
-    if not user_exists:
+    if not user_exists(username):
         # Take to sign_in page
         return
+    
+    
+    
+
 
 
 def sign_in():
