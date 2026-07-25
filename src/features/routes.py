36d83b6a66461a5d9ab242a389e4a10c4294a828
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-07-25 08:59:11 -0500

    Linked and modified css (expenses & home)

--- src/features/routes.py
+++ src/features/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template, Blueprint
+import os
 
-features_bp = Blueprint("features", __name__, template_folder="templates")
+CURR_DIR_PATH = os.path.dirname(__file__)
+
+features_bp = Blueprint("features", __name__, template_folder="templates", static_folder="static",
+                        static_url_path=os.path.join(CURR_DIR_PATH, "statics"))
 
 @features_bp.route("/expenses")
 def expenses():
-    return render_template("expenses.html")
+    headers = ["id", "name", "random thing"]
+    return render_template("expenses.html", table_headers=headers)
 
