39fc653062e1bd682feb39bfa96b6b7eaad65c4c
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-08-08 09:46:41 -0500

    Improved string_to_date filter

--- src/features/utils.py
+++ src/features/utils.py
@@ -9,12 +9,32 @@ def get_year_range():
 
     return years
 
+# Pass month year and day\
+'''
+format = ""
+if not month or year or day:
+    adjust format
 
-def string_to_date(date: str, date_format="%m/%d/%Y"):
+date with values
 
-    datetime_obj = datetime.strptime(date, date_format)
-    date_obj = datetime_obj.date()
+'''
+def string_to_date(year=None, month=None, day=None):  
+    # TODO improve filter
+        
+    if year and month and day:
+        year = int(year)
+        month = int(month)
+        day = int(day)
 
-    return date_obj
+        return date(year, month, day) 
 
+    if year and month:
+        year = int(year)
+        month = int(month)
 
+        return date(year, month, 1)
+    
+    if year:
+        year = int(year)
+        return date(year, 1, 1)
+     
